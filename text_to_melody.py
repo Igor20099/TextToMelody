dc73cc89d6e46a2feb8b5f0b574b496098b5fe17
Igor <megakill123@yandex.ru>
2018-07-13 00:14:31 +0500

    v0.2.Enter 'exit' exit program

--- text_to_melody.py
+++ text_to_melody.py
@@ -7,16 +7,19 @@ pygame.midi.init()
 player = pygame.midi.Output(0)
 player.set_instrument(0)
 
-text = input('Введите текст: ')
-note = []
-for x in text:
-    note.append(ord(x)-50)
-print(note)
-for n in note:
-   # n -=50
-    player.note_on(n,127)
-    time.sleep(0.2)
-    player.note_off(n,127)
+while True:
+    text = input('Введите текст: ')
+    if text == 'exit':
+        break
+    note = []
+    for x in text:
+        note.append(ord(x)-50)
+    print(note)
+    for n in note:
+    # n -=50
+        player.note_on(n,127)
+        time.sleep(0.2)
+        player.note_off(n,127)
 
 
 
